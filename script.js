aab1e3e97974a180dec6b062401a9a1daa7aef8b
shivamacharlaf9 <227r1a05f9@cmrtc.ac.in>
2025-07-04 15:31:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ document.querySelectorAll('nav a').forEach(anchor => {
   });
 });
 
+<script>
+  const hamburger = document.getElementById('hamburger');
+  const navbar = document.getElementById('navbar');
+
+  hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+</script>
+
+
 class TxtType {
   constructor(el, toRotate, period) {
     this.toRotate = toRotate;
